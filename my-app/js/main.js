11fdaa73063b4047f32c1d7a19b499fb0252e9a3
Federico <federicobartoli@me.com>
2020-04-02 15:01:01 +0200

    Menù sticky con javascript

--- my-app/js/main.js
+++ my-app/js/main.js
@@ -1,3 +1,23 @@
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = $('#myHeader');
+
+// Get the offset position of the navbar
+var sticky = $('#myHeader').offset();
+
+console.log(window.pageYOffset);
+console.log(sticky);
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-     alert('ciao');
-};
+  if (window.pageYOffset > 350) {
+   $('#myHeader').addClass('sticky');
+   $('#myHeader').addClass('border');
+   $('.row').removeClass('border');
+    console.log('if');
+  } else {
+   $('#myHeader').removeClass('sticky');
+  console.log('else');
+  }
+}
